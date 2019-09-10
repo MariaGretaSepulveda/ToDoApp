f168588df1e2191f2696dfee07e89744eb20512b
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2019-09-09 20:35:57 -0500

    updates made Todo APP

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Header = (props) => {
+    return (
+        <div>
+
+            <h1> To Do App </h1>
+            <h4>{props.subtitle}</h4>
+        </div>
+    )
+}
+
+
+
+
+export default Header
